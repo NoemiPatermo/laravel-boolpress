65b98ecc328e64afc1e1bcaab6fcb555dde2b1a7
noemi86-cpu <noemipatermo@gmail.com>
2021-09-13 19:33:40 +0200

    move to db

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker; //richiami il faker dentro seeder 
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker) //crea i contenuti usando il faker inserendolo come argomento obbligatoriamente
+    {
+       for ($i = 0; $i < 50; $i++){
+           $postObject = new Post();
+           $postObject->title = $faker->sentence(5);
+           $postObject->cover = $faker->imageUrl(640, 480, 'post', true);
+           $postObject->author = $faker->words(3, true);
+           $postObject->text = $faker->text(100);
+           $postObject->date = $faker->dateTime();
+           $postObject->save();
+       }
+    }
+}
