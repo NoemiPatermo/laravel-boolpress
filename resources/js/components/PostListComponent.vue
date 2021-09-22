23cafa9fa2883015e58494e507bdaca44b324e3a
noemi86-cpu <noemipatermo@gmail.com>
2021-09-22 14:09:56 +0200

    push

--- resources/js/components/PostListComponent.vue
+++ resources/js/components/PostListComponent.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">qui va il v-for</div>
+                    <div class="card-header"> v-for</div>
 
                     <div class="card-body">
                         POST
